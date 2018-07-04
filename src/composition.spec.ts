4454a7887cf136921207d73444edcdc9ce5d10c3
Nick Nance <nance.nick@gmail.com>
2018-07-04 15:00:40 -0700

    add tslint and reformat code

--- src/composition.spec.ts
+++ src/composition.spec.ts
@@ -1,88 +1,87 @@
 import {
     describe,
-    before,
     it,
-} from 'mocha'
+} from "mocha";
 
 import {
     expect,
-} from 'chai'
+} from "chai";
 
 import {
+    availablePrices,
+    averageDollarValue,
+    fastestCar,
     isLastInStock,
     nameOfFirstCar,
-    averageDollarValue,
     sanitizeNames,
-    availablePrices,
-    fastestCar,
-} from './composition'
+} from "./composition";
 
 // Example Data
 const CARS = [
     {
-        name: "Ferrari FF",
-        horsepower: 660,
         dollar_value: 700000,
-        in_stock: true
+        horsepower: 660,
+        in_stock: true,
+        name: "Ferrari FF",
     },
     {
-        name: "Spyker C12 Zagato",
-        horsepower: 650,
         dollar_value: 648000,
-        in_stock: false
+        horsepower: 650,
+        in_stock: false,
+        name: "Spyker C12 Zagato",
     },
     {
-        name: "Jaguar XKR-S",
-        horsepower: 550,
         dollar_value: 132000,
-        in_stock: false
+        horsepower: 550,
+        in_stock: false,
+        name: "Jaguar XKR-S",
     },
     {
-        name: "Audi R8",
-        horsepower: 525,
         dollar_value: 114200,
-        in_stock: false
+        horsepower: 525,
+        in_stock: false,
+        name: "Audi R8",
     },
     {
-        name: "Aston Martin One-77",
-        horsepower: 750,
         dollar_value: 1850000,
-        in_stock: true
+        horsepower: 750,
+        in_stock: true,
+        name: "Aston Martin One-77",
     },
     {
-        name: "Pagani Huayra",
-        horsepower: 700,
         dollar_value: 1300000,
-        in_stock: false
-    }
-]
+        horsepower: 700,
+        in_stock: false,
+        name: "Pagani Huayra",
+    },
+];
 
 const santized = [
-    'ferrari_ff',
-    'spyker_c12_zagato',
-    'jaguar_xkr_s',
-    'audi_r8',
-    'aston_martin_one_77',
-    'pagani_huayra'
-]
+    "ferrari_ff",
+    "spyker_c12_zagato",
+    "jaguar_xkr_s",
+    "audi_r8",
+    "aston_martin_one_77",
+    "pagani_huayra",
+];
 
-describe('when using composition', () => {
-    it('the last car should not be in stock', () => {
-        expect(isLastInStock(CARS)).to.be.false
-    })
-    it('name of the first car should be Ferrari FF', () => {
-        expect(nameOfFirstCar(CARS))
-    })
-    it('average dollar value should equal 790700', () => {
-        expect(averageDollarValue(CARS)).to.eq(790700)
-    })
-    it('sanatized names should be lower case with underscores', () => {
-        expect(sanitizeNames(CARS)).to.deep.equal(santized)
-    })
-    it('available prices should be formated', () => {
-        expect(availablePrices(CARS)).to.equal('$700,000.00, $1,850,000.00')
-    })
-    it('fastest car should be the Ason Martin ONe-77', () => {
-        expect(fastestCar(CARS)).to.equal('Aston Martin One-77 is the fastest')
-    })
-})
+describe("when using composition", () => {
+
+    it("the last car should not be in stock", () =>
+        expect(isLastInStock(CARS)).to.be.false);
+
+    it("name of the first car should be Ferrari FF", () =>
+        expect(nameOfFirstCar(CARS)));
+
+    it("average dollar value should equal 790700", () =>
+        expect(averageDollarValue(CARS)).to.eq(790700));
+
+    it("sanatized names should be lower case with underscores", () =>
+        expect(sanitizeNames(CARS)).to.deep.equal(santized));
+
+    it("available prices should be formated", () =>
+        expect(availablePrices(CARS)).to.equal("$700,000.00, $1,850,000.00"));
+
+    it("fastest car should be the Ason Martin ONe-77", () =>
+        expect(fastestCar(CARS)).to.equal("Aston Martin One-77 is the fastest"));
+});
--- src/composition.ts
+++ src/composition.ts
@@ -1,57 +1,57 @@
+import accounting from "accounting";
 import {
+  add,
   compose,
+  filter,
+  head,
+  join,
+  last,
+  map,
   prop,
   reduce,
-  add,
-  map,
-  join,
-  filter,
-  last,
-  head,
   replace,
-  toLower,
   sortBy,
-} from "ramda"
-import accounting from "accounting"
+  toLower,
+} from "ramda";
 
 interface ICar {
-    name: string
-    horsepower: number
-    dollar_value: number
-    in_stock: boolean
+    name: string;
+    horsepower: number;
+    dollar_value: number;
+    in_stock: boolean;
 }
 
 // Exercise 1:
 // ============
 // Use _.compose() to rewrite the function below. Hint: _.prop() is curried.
-export const isLastInStock = compose<ICar[], ICar, boolean>(prop("in_stock"), last)
+export const isLastInStock = compose<ICar[], ICar, boolean>(prop("in_stock"), last);
 
 // Exercise 2:
 // ============
 // Use _.compose(), _.prop() and _.head() to retrieve the name of the first car.
-export const nameOfFirstCar = compose<ICar[], ICar, string>(prop("name"), head)
+export const nameOfFirstCar = compose<ICar[], ICar, string>(prop("name"), head);
 
 // Exercise 3:
 // ============
 // Use the helper function _average to refactor averageDollarValue as a composition.
-const _average = (xs: number[]) => reduce<number, number>(add, 0, xs) / xs.length
+const average = (xs: number[]) => reduce<number, number>(add, 0, xs) / xs.length;
 // <- leave be
 
-export const averageDollarValue = compose<ICar[], number[], number>(_average, map(c => c.dollar_value))
-
+export const averageDollarValue = compose<ICar[], number[], number>(average, map((c) => c.dollar_value));
 
 // Exercise 4:
 // ============
-// Write a function: sanitizeNames() using compose that returns a list of lowercase and underscored car's names: e.g: sanitizeNames([{name: 'Ferrari FF', horsepower: 660, dollar_value: 700000, in_stock: true}]) ”
-//=> ['ferrari_ff'].
+// Write a function: sanitizeNames() using compose that returns a list of lowercase and \
+// underscored car's names: e.g:
+// sanitizeNames([{name: 'Ferrari FF', horsepower: 660, dollar_value: 700000, in_stock: true}]) => ['ferrari_ff'].
 
-const _underscore = replace(/\W+/g, "_") //<-- leave this alone and use to sanitize
+const underscore = replace(/\W+/g, "_"); // <-- leave this alone and use to sanitize
 
 export const sanitizeNames = map(compose<ICar, string, string, string>(
-    _underscore,
+    underscore,
     toLower,
-    prop("name"))
-)
+    prop("name")),
+);
 
 // Bonus 1:
 // ============
@@ -59,9 +59,9 @@ export const sanitizeNames = map(compose<ICar, string, string, string>(
 
 export const availablePrices = compose<ICar[], ICar[], string[], string>(
     join(", "),
-    map(x => accounting.formatMoney(x.dollar_value)),
-    filter<ICar>(prop("in_stock"))
-)
+    map((x) => accounting.formatMoney(x.dollar_value)),
+    filter<ICar>(prop("in_stock")),
+);
 
 // Bonus 2:
 // ============
@@ -71,5 +71,5 @@ export const fastestCar = compose<ICar[], ICar[], ICar, string, string>(
     (name) => `${name} is the fastest`,
     prop("name"),
     last,
-    sortBy(car => car.horsepower)
-)
+    sortBy((car) => car.horsepower),
+);
